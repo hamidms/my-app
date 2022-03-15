e817e191951705387cec7fe0389984f6be3c5ac7
hamidmsukardi <hamidmsukardi@gmail.com>
2022-03-15 20:09:18 +0700

    test style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import Header from '../components/Header';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return(
     <>
       <Header />
-      <h1>Welcome Hamid</h1>
+      <h1 className={styles['title-homepage']}>Welcome Hamid</h1>
     </>
   );
 }
